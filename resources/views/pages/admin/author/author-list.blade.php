b4594f8d7c00e8b613ddcfb5791f4c08d58a8a3e
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-06-19 15:58:07 +0700

    list author

--- resources/views/pages/admin/author/author-list.blade.php
+++ resources/views/pages/admin/author/author-list.blade.php
@@ -101,7 +101,7 @@
                     <td>
                         <div class="d-flex">
 
-                            <button data-bs-toggle="tooltip" title="Detail" data-id="{{ $author->id }}" data-name="{{ $author->user->name }}" data-image="{{ $author->user->image ? 'storage/'.$author->user->image : "default.png" }}" data-email="{{ $author->user->email }}" class="btn btn-sm btn-detail btn-primary me-2" style="background-color:#5D87FF">
+                            <button data-bs-toggle="tooltip" title="Detail" data-id="{{ $author->id }}" data-name="{{ $author->user->name }}" data-birth="{{ $author->user->date_of_birth ? $author->user->date_of_birth : "-" }}" data-address="{{ $author->user->address ? $author->user->address : "-" }}" data-image="{{ $author->user->image ? 'storage/'.$author->user->image : "default.png" }}" data-email="{{ $author->user->email }}" class="btn btn-sm btn-detail btn-primary me-2" style="background-color:#5D87FF">
                                 <i><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24">
                                         <path fill="currentColor" d="M12 6.5a9.77 9.77 0 0 1 8.82 5.5c-1.65 3.37-5.02 5.5-8.82 5.5S4.83 15.37 3.18 12A9.77 9.77 0 0 1 12 6.5m0-2C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5m0 5a2.5 2.5 0 0 1 0 5a2.5 2.5 0 0 1 0-5m0-2c-2.48 0-4.5 2.02-4.5 4.5s2.02 4.5 4.5 4.5s4.5-2.02 4.5-4.5s-2.02-4.5-4.5-4.5" />
                                     </svg></i>
@@ -258,21 +258,21 @@
                 <!-- Modal body -->
                 <div class="modal-body">
                     <div class="d-flex justify-content-center">
-                        <img class="rounded-circle mb-4" id="detail-image" width="150" alt="photo-siswa" height="150" />
+                        <img class="rounded-circle mb-4" id="detail-image" width="150" alt="photo" height="150" />
                     </div>
                     <div class="container-fluid">
                         <div class="row">
                             <div class="col">
                                 <ul class="list-group list-group-flush">
-                                    <li class="list-group-item" style="font-weight: bold;">Nama : {{ $author->user->name }}<span id="detail-name" style="font-weight: normal;"></span>
+                                    <li class="list-group-item" style="font-weight: bold;">Nama :<span id="detail-name" style="font-weight: normal;"></span>
                                     </li>
-                                    <li class="list-group-item" style="font-weight: bold;">Email: {{ $author->user->email }}<span id="detail-email" style="font-weight: normal;"></span>
+                                    <li class="list-group-item" style="font-weight: bold;">Email:<span id="detail-email" style="font-weight: normal;"></span>
                                     </li>
                                 </ul>
                             </div>
                             <div class="col">
                                 <ul class="list-group list-group-flush">
-                                    <li class="list-group-item" style="font-weight: bold;">Tanggal Lahir: {{ $author->user->date_of_birth ? $author->user->date_of_birth : "-" }}<span id="detail-birth_date" style="font-weight: normal;"></span></li>
+                                    <li class="list-group-item" style="font-weight: bold;">Tanggal Lahir: <span id="detail-birth_date" style="font-weight: normal;"></span></li>
                                     <li class="list-group-item" style="font-weight: bold;">Alamat: {{ $author->user->address ? $author->user->address : "-"}}<span id="detail-address" style="font-weight: normal;"></span></li>
                                 </ul>
                             </div>
@@ -355,11 +355,15 @@
         var name = $(this).data('name');
         var email = $(this).data('email');
         var image = $(this).data('image');
+        var date = $(this).data('data_of_birth')
+        var address = $(this).data('address')
         $('#form-tolak').attr('action', '/confirm-author/' + id);
         $('#form-terima').attr('action', '/confirm-author/' + id);
         $('#detail-name').val(name);
         $('#detail-email').val(email);
-        $('#detail-image').attr('src', image);;
+        $('#detail-image').attr('src', image);
+        $('#detail-birth_date').val(date)
+        $('#detail-address').val(address)
         console.log(id);
         $('#modal-detail').modal('show');
     });
