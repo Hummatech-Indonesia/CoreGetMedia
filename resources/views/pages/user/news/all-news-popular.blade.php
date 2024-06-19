6af47e7b8539a5636469136dd89802e8146db53c
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2024-06-19 11:23:14 +0700

    Update all-news-popular.blade.php

--- resources/views/pages/user/news/all-news-popular.blade.php
+++ resources/views/pages/user/news/all-news-popular.blade.php
@@ -21,7 +21,6 @@
         height: 250px;
         object-fit: cover;
     }
-
 </style>
 @endsection
 
@@ -38,14 +37,16 @@
 
 
 <div class="">
-    <div class="modal fade searchModal" id="searchModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-hidden="true">
+    <div class="modal fade searchModal" id="searchModal" data-bs-backdrop="static" data-bs-keyboard="false"
+        tabindex="-1" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <form>
                     <input type="text" class="form-control" placeholder="Search here....">
                     <button type="submit"><i class="fi fi-rr-search"></i></button>
                 </form>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"><i class="ri-close-line"></i></button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"><i
+                        class="ri-close-line"></i></button>
             </div>
         </div>
     </div>
@@ -58,63 +59,71 @@
                 <div class="col-lg-8">
                     <div class="row">
                         {{-- @forelse ($newsByDate as $item)
-                @php
-                    $dateParts = date_parse($item->upload_date);
-                @endphp --}}
-                        @forelse ($popular as $popular)
+                        @php
+                        $dateParts = date_parse($item->upload_date);
+                        @endphp --}}
+                        @forelse ($popular as $item)
 
-                        <div class="col-md-6">
-                            <div class="news-card-six">
-                                <div class="news-card-img">
-                                    <img src="{{ asset('storage/' . $popular->image) }}" alt="" class="img-all">
-                                    {{-- @foreach ($subCategories as $subCategory)
-                            <p class="tag">
-                                <a href="{{ route('categories.show.user', ['category' => $item->newsCategories[0]->category->slug]) }}" class="news-cat">{{ $item->newsCategories[0]->category->name }}</a>
-                                    </p>
-                                    @endforeach --}}
-                                    @foreach ($subCategories as $subCategory)
-                                    <p class="tag">
-                                        <a href="{{ route('categories.show.user', ['category' => $popular->newsCategories[0]->category->slug]) }}" class="news-cat">{{ $popular->newsCategories[0]->category->name }}</a>
-                                    </p>
-                                    @endforeach
-                                </div>
-                                <div class="news-card-info">
+                            <div class="col-md-6">
+                                <div class="news-card-six">
+                                    <div class="news-card-img">
+                                        <img src="{{ asset('storage/' . $item->image) }}" alt="" class="img-all"
+                                            style="width: 100%; height: auto;">
+                                        {{-- @foreach ($subCategories as $subCategory)
+                                        <p class="tag">
+                                            <a href="{{ route('categories.show.user', ['category' => $item->newsCategories[0]->category->slug]) }}"
+                                                class="news-cat">{{ $item->newsCategories[0]->category->name }}</a>
+                                        </p>
+                                        @endforeach --}}
+                                        @foreach ($subCategories as $subCategory)
+                                            <p class="tag">
+                                                <a href="{{ route('categories.show.user', ['category' => $item->newsCategories[0]->category->slug]) }}"
+                                                    class="news-cat">{{ $item->newsCategories[0]->category->name }}</a>
+                                            </p>
+                                        @endforeach
+                                    </div>
+                                    <div class="news-card-info">
 
-                                    <h3><a href="{{ route('news.singlepost', $popular->slug) }}">{{ $popular->name }}</a>
-                                    </h3>
-                                    <ul class="news-metainfo list-style">
-                                        <li><i class="fi fi-rr-calendar-minus"></i><a href="javascript:void(0)">{{ \Carbon\Carbon::parse($popular->date)->translatedFormat('d F Y') }}</a>
-                                        </li>
-                                        <li><i class="fi fi-rr-eye"></i>{{$popular->newsViews->count()}} x dilihat
-                                        </li>
-                                        {{-- <li>
-                                        <button type="submit" style="background: transparent;border:transparent" class="like">
-                                            <svg class="last mb-1" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24">
-                                                <path fill="#E93314" d="M18 21H7V8l7-7l1.25 1.25q.175.175.288.475t.112.575v.35L14.55 8H21q.8 0 1.4.6T23 10v2q0 .175-.05.375t-.1.375l-3 7.05q-.225.5-.75.85T18 21m-9-2h9l3-7v-2h-9l1.35-5.5L9 8.85zM9 8.85V19zM7 8v2H4v9h3v2H2V8z" />
-                                            </svg>
-                                        </button>
-                                    </li> --}}
-                                    </ul>
+                                        <h3><a href="{{ route('news.singlepost', $item->slug) }}">{{ $item->name }}</a>
+                                        </h3>
+                                        <ul class="news-metainfo list-style">
+                                            <li><i class="fi fi-rr-calendar-minus"></i><a
+                                                    href="javascript:void(0)">{{ \Carbon\Carbon::parse($item->date)->translatedFormat('d F Y') }}</a>
+                                            </li>
+                                            <li><i class="fi fi-rr-eye"></i>{{$item->newsViews->count()}} x dilihat
+                                            </li>
+                                            {{-- <li>
+                                                <button type="submit" style="background: transparent;border:transparent"
+                                                    class="like">
+                                                    <svg class="last mb-1" xmlns="http://www.w3.org/2000/svg" width="18"
+                                                        height="18" viewBox="0 0 24 24">
+                                                        <path fill="#E93314"
+                                                            d="M18 21H7V8l7-7l1.25 1.25q.175.175.288.475t.112.575v.35L14.55 8H21q.8 0 1.4.6T23 10v2q0 .175-.05.375t-.1.375l-3 7.05q-.225.5-.75.85T18 21m-9-2h9l3-7v-2h-9l1.35-5.5L9 8.85zM9 8.85V19zM7 8v2H4v9h3v2H2V8z" />
+                                                    </svg>
+                                                </button>
+                                            </li> --}}
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
                         @empty
 
-                        <div class="d-flex justify-content-center">
-                            <div>
-                                <img src="{{ asset('assets/img/no-data.svg') }}" alt="">
+                            <div class="d-flex justify-content-center">
+                                <div>
+                                    <img src="{{ asset('assets/img/no-data.svg') }}" alt="">
+                                </div>
                             </div>
-                        </div>
-                        <div class="text-center">
-                            <h4>Tidak ada data</h4>
-                        </div>
+                            <div class="text-center">
+                                <h4>Tidak ada data</h4>
+                            </div>
                         @endforelse
                     </div>
 
-                    {{-- <div class="text-center item-center mt-4 d-flex justify-content-center" style="background-color:#F6F6F6; width:100%;height:200px;">
-                <h5 class="mt-5 text-dark">Iklan</h5>
-            </div> --}}
+                    {{-- <div class="text-center item-center mt-4 d-flex justify-content-center"
+                        style="background-color:#F6F6F6; width:100%;height:200px;">
+                        <h5 class="mt-5 text-dark">Iklan</h5>
+                    </div> --}}
                     {{-- <x-paginator :paginator="$newsByDate" /> --}}
                 </div>
 
@@ -124,8 +133,11 @@
                             <h3 class="sidebar-widget-title">Kategori Populer</h3>
                             <ul class="category-widget list-style">
                                 @foreach ($CategoryPopulars as $category)
-                                <li><a data-toggle="tooltip" data-placement="top" title="{{ $category->name }}" href="{{ route('categories.show.user', ['category' => $category->slug]) }}"><img src="{{ asset('assets/img/icons/arrow-right.svg') }}" alt="Image">{{ $category->name }}
-                                        <span>({{ $category->news_categories_count }})</span></a></li>
+                                    <li><a data-toggle="tooltip" data-placement="top" title="{{ $category->name }}"
+                                            href="{{ route('categories.show.user', ['category' => $category->slug]) }}"><img
+                                                src="{{ asset('assets/img/icons/arrow-right.svg') }}"
+                                                alt="Image">{{ $category->name }}
+                                            <span>({{ $category->news_categories_count }})</span></a></li>
                                 @endforeach
                             </ul>
                         </div>
@@ -134,7 +146,7 @@
                             <h3 class="sidebar-widget-title">Tag Populer</h3>
                             <ul class="tag-list list-style">
                                 @forelse ($popularTags as $popularTag)
-                                <li><a href="{{ route('list-tag.user') }}">{{ $popularTag->name }}</a></li>
+                                    <li><a href="{{ route('list-tag.user') }}">{{ $popularTag->name }}</a></li>
                                 @empty
                                 @endforelse
                             </ul>
@@ -146,4 +158,5 @@
 
     </div>
 </div>
+
 @endsection
