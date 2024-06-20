c5b6ccdced8cb5a2230edb5a4d046160a640cc6a
nesaathussholeha <nesaathussholeha@gmail.com>
2024-06-20 16:25:16 +0700

    Update set-price.blade.php

--- resources/views/pages/admin/advertisement/set-price.blade.php
+++ resources/views/pages/admin/advertisement/set-price.blade.php
@@ -22,9 +22,9 @@
 
 <h3 class="mb-4">Pilih Halaman</h3>
     <div class="card">
-        <div class="card-body">
+        <div class="p-2">
 
-            <ul class="nav nav-tabs mb-5" role="tablist">
+            <ul class="nav nav-tabs" role="tablist">
                 <li class="nav-item" role="presentation">
                     <a class="nav-link active" data-bs-toggle="tab" href="#navpill-111" role="tab" aria-selected="true">
                         <span>Beranda</span>
@@ -94,20 +94,125 @@
 
         <!-- Tab Diterima -->
         <div id="navpill-222" class="tab-pane fade" role="tabpanel">
-            <h3>Halaman Diterima</h3>
-            <p>Isi konten untuk tab Diterima di sini.</p>
+            <h3 class="mb-3">Keterangan Iklan</h3>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h6 class="mb-3">Keterangan posisi iklan & ukuran</h6>
+                            <h4 class="mb-3">Posisi Kanan (1000 x 1000)</h4>
+                            <h6>Preview posisi</h6>
+                            <img src="{{ asset('assets/iklan.png') }}" alt="Nama Gambar" class="img-fluid mt-2 mb-4"
+                                style="max-width: 250px; border: none;">
+                            <h6 class="mb-2">Harga :</h6>
+                            <h4 class="mb-4">Rp. 100.000</h4>
+
+                            <div class="d-flex justify-content-between">
+                                <button type="button"
+                                    class="btn mb-1 waves-effect waves-light btn-warning w-100"
+                                    style="background-color: #FFD643; border: none">
+                                    Edit
+                                    <i class="ti ti-edit"></i>
+                                </button>
+                                <button type="button"
+                                    class="btn mb-1 waves-effect waves-light btn-danger w-100 ms-4">
+                                    Hapus
+                                    <i class="ti ti-trash"></i>
+                                </button>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <!-- Card 2 -->
+                </div>
+                <div class="col-md-4">
+                    <!-- Card 3 -->
+                </div>
+            </div>
         </div>
 
         <!-- Tab Pending -->
         <div id="navpill-333" class="tab-pane fade" role="tabpanel">
-            <h3>Halaman Pending</h3>
-            <p>Isi konten untuk tab Pending di sini.</p>
+            <h3 class="mb-3">Keterangan Iklan</h3>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h6 class="mb-3">Keterangan posisi iklan & ukuran</h6>
+                            <h4 class="mb-3">Posisi Kanan (1000 x 1000)</h4>
+                            <h6>Preview posisi</h6>
+                            <img src="{{ asset('assets/iklan.png') }}" alt="Nama Gambar" class="img-fluid mt-2 mb-4"
+                                style="max-width: 250px; border: none;">
+                            <h6 class="mb-2">Harga :</h6>
+                            <h4 class="mb-4">Rp. 100.000</h4>
+
+                            <div class="d-flex justify-content-between">
+                                <button type="button"
+                                    class="btn mb-1 waves-effect waves-light btn-warning w-100"
+                                    style="background-color: #FFD643; border: none">
+                                    Edit
+                                    <i class="ti ti-edit"></i>
+                                </button>
+                                <button type="button"
+                                    class="btn mb-1 waves-effect waves-light btn-danger w-100 ms-4">
+                                    Hapus
+                                    <i class="ti ti-trash"></i>
+                                </button>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <!-- Card 2 -->
+                </div>
+                <div class="col-md-4">
+                    <!-- Card 3 -->
+                </div>
+            </div>
         </div>
 
         <!-- Tab Ditolak -->
         <div id="navpill-444" class="tab-pane fade" role="tabpanel">
-            <h3>Halaman Ditolak</h3>
-            <p>Isi konten untuk tab Ditolak di sini.</p>
+            <h3 class="mb-3">Keterangan Iklan</h3>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h6 class="mb-3">Keterangan posisi iklan & ukuran</h6>
+                            <h4 class="mb-3">Posisi Kanan (1000 x 1000)</h4>
+                            <h6>Preview posisi</h6>
+                            <img src="{{ asset('assets/iklan.png') }}" alt="Nama Gambar" class="img-fluid mt-2 mb-4"
+                                style="max-width: 250px; border: none;">
+                            <h6 class="mb-2">Harga :</h6>
+                            <h4 class="mb-4">Rp. 100.000</h4>
+
+                            <div class="d-flex justify-content-between">
+                                <button type="button"
+                                    class="btn mb-1 waves-effect waves-light btn-warning w-100"
+                                    style="background-color: #FFD643; border: none">
+                                    Edit
+                                    <i class="ti ti-edit"></i>
+                                </button>
+                                <button type="button"
+                                    class="btn mb-1 waves-effect waves-light btn-danger w-100 ms-4">
+                                    Hapus
+                                    <i class="ti ti-trash"></i>
+                                </button>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <!-- Card 2 -->
+                </div>
+                <div class="col-md-4">
+                    <!-- Card 3 -->
+                </div>
+            </div>
         </div>
     </div>
 
