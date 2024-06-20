f6502b0ba768d9eec0aa8f3242b1770be3ea378d
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-06-20 10:56:52 +0700

    fetch unggah berita author

--- resources/views/layouts/author/sidebar.blade.php
+++ resources/views/layouts/author/sidebar.blade.php
@@ -49,13 +49,13 @@
                     <span class="hide-menu text-white">Berita</span>
                 </li>
 
-                {{-- <li class="sidebar-item">
+                <li class="sidebar-item">
                     <a class="sidebar-link {{ request()->routeIs('create.news' ? 'active' : '') }}"
                         href="{{ route('create.news') }}" aria-expanded="false">
                         <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24"><path fill="currentColor" d="M11 13H5v-2h6V5h2v6h6v2h-6v6h-2z"/></svg>
-                        <span class="hide-menu">Upload Berita</span>
+                        <span class="hide-menu">Unggah Berita</span>
                     </a>
-                </li> --}}
+                </li>
 
                 <li class="sidebar-item">
                     <a class="sidebar-link"
