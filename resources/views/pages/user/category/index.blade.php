d5c3ec62dccf8e00fe80c4fa8c7a416b14d2d285
mrizkibahtiar <muhammadrizkibaht@gmail.com>
2024-06-20 09:04:03 +0700

    Update index.blade.php

--- resources/views/pages/user/category/index.blade.php
+++ resources/views/pages/user/category/index.blade.php
@@ -182,7 +182,7 @@
                             </h3>
                             @forelse ($trending_news as $trending)
                             @if ($trending->news_views_count > 0)
-                            <div class="news-card-three">
+                            <div class="news-card-three gap-2">
                                 <div class="news-card-img" class="">
                                     <img src="{{ asset('storage/' . $trending->image) }}" class="img-popular"
                                         alt="Image" />
