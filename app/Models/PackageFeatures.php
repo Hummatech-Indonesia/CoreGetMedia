c529797b06a74a6283e021ef582f5a3c79f0674e
Abdul Kader <abdulkader0126@gmail.com>
2024-06-20 09:49:58 +0700

    paket and feature paket

---
+++ app/Models/PackageFeatures.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PackageFeatures extends Model
+{
+    use HasFactory;
+}
