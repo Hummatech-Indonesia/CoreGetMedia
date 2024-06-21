948566d06d3d7ac9e008aa0a4c96bcbbeb06c83b
nesaathussholeha <nesaathussholeha@gmail.com>
2024-06-21 13:17:01 +0700

    share whatsapp - singlepost

--- resources/views/pages/user/singlepost/index.blade.php
+++ resources/views/pages/user/singlepost/index.blade.php
@@ -48,18 +48,19 @@
             margin-top: 10px;
         }
     </style>
-        <style>
-            .tooltip-custom {
-                position: absolute;
-                background-color: #333;
-                color: #fff;
-                padding: 5px 10px;
-                border-radius: 5px;
-                font-size: 12px;
-                display: none;
-                z-index: 1000;
-            }
-        </style>
+    <style>
+        .tooltip-custom {
+            position: fixed;
+            background-color: #333;
+            color: #fff;
+            padding: 5px 10px;
+            border-radius: 5px;
+            font-size: 12px;
+            display: none;
+            z-index: 1000;
+        }
+
+    </style>
 @endsection
 
 @section('content')
@@ -174,7 +175,7 @@
                         <div>
                             <h1>{{ $news->name }}</h1>
                             <p class="d-flex gap-1">Share :
-                                {{-- <a id="wa" class="logo" data-name="{{ $news->name }}" data-slug="{{ $news->slug }}"
+                                {{-- <a id="wa" class="logo " data-name="{{ $news->name }}" data-slug="{{ $news->slug }}"
                                     href="https://wa.me/NOMOR_TELEPON_ANDA?text=PESAN_ANDA">
                                      <svg height="19" width="19" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
                                           xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 58 58" xml:space="preserve" fill="#000000">
@@ -193,9 +194,23 @@
                                      </svg>
                                  </a> --}}
 
-                                 {{-- <button class="wpbtn" title="Share to WhatsApp" onclick="shareToWhatsApp()">
-                                    <img class="wp" src="https://www.sharethis.com/wp-content/uploads/2017/05/WhatsApp.png" alt="WhatsApp">
-                                </button> --}}
+                                 <button class="wpbtn" style="background-color: transparent; border: none" title="Share to WhatsApp" onclick="shareToWhatsApp()">
+                                    <svg height="19" width="19" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+                                          xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 58 58" xml:space="preserve" fill="#000000">
+                                         <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                         <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                         <g id="SVGRepo_iconCarrier">
+                                             <g>
+                                                 <path style="fill:#2CB742;"
+                                                       d="M0,58l4.988-14.963C2.457,38.78,1,33.812,1,28.5C1,12.76,13.76,0,29.5,0S58,12.76,58,28.5 S45.24,57,29.5,57c-4.789,0-9.299-1.187-13.26-3.273L0,58z">
+                                                 </path>
+                                                 <path style="fill:#FFFFFF;"
+                                                       d="M47.683,37.985c-1.316-2.487-6.169-5.331-6.169-5.331c-1.098-0.626-2.423-0.696-3.049,0.42 c0,0-1.577,1.891-1.978,2.163c-1.832,1.241-3.529,1.193-5.242-0.52l-3.981-3.981l-3.981-3.981c-1.713-1.713-1.761-3.41-0.52-5.242 c0.272-0.401,2.163-1.978,2.163-1.978c1.116-0.627,1.046-1.951,0.42-3.049c0,0-2.844-4.853-5.331-6.169 c-1.058-0.56-2.357-0.364-3.203,0.482l-1.758,1.758c-5.577,5.577-2.831,11.873,2.746,17.45l5.097,5.097l5.097,5.097 c5.577,5.577,11.873,8.323,17.45,2.746l1.758-1.758C48.048,40.341,48.243,39.042,47.683,37.985z">
+                                                 </path>
+                                             </g>
+                                         </g>
+                                     </svg>
+                                </button>
 
                                 {{-- <a id="fb">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" viewBox="0 0 263 263">
@@ -882,13 +897,13 @@
         }
     </script>
 
-    <script>
+<script>
     function shareToWhatsApp() {
-        // Ganti 'Paste Your Message or link here' dengan pesan atau link yang ingin Anda bagikan
-        var message = 'Paste Your Message or link here';
+        var currentUrl = window.location.href;
+
+        var message = 'Check out this page: ' + currentUrl;
         var whatsappUrl = 'whatsapp://send?text=' + encodeURIComponent(message);
 
-        // Buka aplikasi WhatsApp atau tampilkan pesan jika WhatsApp tidak terpasang
         window.location.href = whatsappUrl;
     }
 </script>
