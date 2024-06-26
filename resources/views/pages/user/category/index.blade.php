696b894ad9821fcb8bbdd850728d8241be20bcc6
ArdianS18 <ardiansupriadi464@gmail.com>
2024-06-26 08:46:00 +0700

    Update index.blade.php

--- resources/views/pages/user/category/index.blade.php
+++ resources/views/pages/user/category/index.blade.php
@@ -130,7 +130,7 @@
                                     Illuminate\Support\Str::limit($new->name, $limit = 170, $end = '...') !!}
                                 </a>
                             </h3>
-                            <p>{!! Illuminate\Support\Str::limit($new->description, $limit = 100, $end = '...') !!}</p>
+                            <p>{!! Illuminate\Support\Str::limit($new->description, $limit = 170, $end = '...') !!}</p>
                             <ul class="news-metainfo list-style">
                                 <li><i class="fi fi-rr-calendar-minus"></i><a href="javascript:void(0)">{{ \Carbon\Carbon::parse($new->created_at)->translatedFormat('d F Y') }}</a>
                                 </li>
