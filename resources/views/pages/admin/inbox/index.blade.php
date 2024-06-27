c68007bcc9c1d7240f5b605dd783903002e72ccb
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-06-27 18:40:49 +0700

    Update index.blade.php

--- resources/views/pages/admin/inbox/index.blade.php
+++ resources/views/pages/admin/inbox/index.blade.php
@@ -57,7 +57,7 @@
                     @forelse ($articles as $article)
                         <tr>
                             <td>
-                                @if ($article->user->image != null && Storage::disk('public')->exists($article->user->image))
+                                @if (!$article->user->image == null && Storage::disk('public')->exists($article->user->image))
                                     <img src="{{ asset('storage/'. $article->user->image)}}" class="rounded-circle me-2 user-profile" style="object-fit: cover" width="35" height="35" alt="" />
                                 @else
                                     <img src="{{ asset('default.png') }}" class="rounded-circle me-2 user-profile" style="object-fit: cover" width="35" height="35" alt="" />
