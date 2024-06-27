6a711f17a0a73b38bec99a7d24aa18e33528e6a5
13Farahamalia <140934377+13Farahamalia@users.noreply.github.com>
2024-06-27 12:05:57 +0700

    Update index.blade.php

--- resources/views/pages/user/category/index.blade.php
+++ resources/views/pages/user/category/index.blade.php
@@ -202,16 +202,6 @@
                                 </div>
                             </div>
                             @else
-                                <div class="col-12">
-                                    <div class="d-flex justify-content-center">
-                                        <div>
-                                            <img src="{{ asset('assets/img/no-data/empty.png') }}" width="150px" alt="">
-                                        </div>
-                                    </div>
-                                    <div class="text-center">
-                                        <h5>Tidak ada data</h5>
-                                    </div>
-                                </div>
                             @endif
                         @empty
                         <div class="col-12">
