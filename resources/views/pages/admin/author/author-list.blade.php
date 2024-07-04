8477caad6cd8ec0e9df251157e871f72a8691100
jovitamaharani <141116867+jovitamaharani@users.noreply.github.com>
2024-07-04 11:53:19 +0700

    Update author-list.blade.php

--- resources/views/pages/admin/author/author-list.blade.php
+++ resources/views/pages/admin/author/author-list.blade.php
@@ -101,7 +101,7 @@
                     <td>
                         <div class="d-flex">
 
-                            <button data-bs-toggle="tooltip" title="Detail" data-id="{{ $author->id }}" data-name="{{ $author->user->name }}" data-birth="{{ $author->user->date_of_birth ? $author->user->date_of_birth : "-" }}" data-address="{{ $author->user->address ? $author->user->address : "-" }}" data-image="{{ $author->user->image ? 'storage/'.$author->user->image : "default.png" }}" data-email="{{ $author->user->email }}" dta-cv="{{ $author->cv }}" class="btn btn-sm btn-detail btn-primary me-2" style="background-color:#5D87FF">
+                            <button data-bs-toggle="tooltip" title="Detail" data-id="{{ $author->id }}" data-name="{{ $author->user->name }}" data-birth="{{ $author->user->date_of_birth ? $author->user->date_of_birth : "-" }}" data-address="{{ $author->user->address ? $author->user->address : "-" }}" data-image="{{ $author->user->image ? 'storage/'.$author->user->image : "default.png" }}" data-email="{{ $author->user->email }}" data-cv="{{ asset('storage/'. $author->cv) }}" class="btn btn-sm btn-detail btn-primary me-2" style="background-color:#5D87FF">
                                 <i><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24">
                                         <path fill="currentColor" d="M12 6.5a9.77 9.77 0 0 1 8.82 5.5c-1.65 3.37-5.02 5.5-8.82 5.5S4.83 15.37 3.18 12A9.77 9.77 0 0 1 12 6.5m0-2C7 4.5 2.73 7.61 1 12c1.73 4.39 6 7.5 11 7.5s9.27-3.11 11-7.5c-1.73-4.39-6-7.5-11-7.5m0 5a2.5 2.5 0 0 1 0 5a2.5 2.5 0 0 1 0-5m0-2c-2.48 0-4.5 2.02-4.5 4.5s2.02 4.5 4.5 4.5s4.5-2.02 4.5-4.5s-2.02-4.5-4.5-4.5" />
                                     </svg></i>
