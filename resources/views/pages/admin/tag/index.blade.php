f4cbcf05c1881b4640d4521d43d2e8423fd3547e
Aryarizki8507 <mbedalitenga@gmail.com>
2024-07-30 09:16:21 +0700

    push

--- resources/views/pages/admin/tag/index.blade.php
+++ resources/views/pages/admin/tag/index.blade.php
@@ -109,11 +109,12 @@
                         </td>
                     </tr>
                 @empty
-                    <tr>
-                        <td class="text-center align-middle" colspan="100%">
-                            Belum ada data
-                        </td>
-                    </tr>
+                <tr>
+                    <td class="text-center align-middle" colspan="100%">
+                        <img src="{{ asset('assets/img/no-data.svg') }}" width="200px" alt="">
+                        <p>Belum ada data</p>
+                    </td>
+                </tr>
                 @endforelse
 
             </tbody>
